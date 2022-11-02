3c5fd491a34228a449592a86e039e3a30d67d1b9
Giulia Girardi <gg.giuliagirardi@gmail.com>
2022-11-02 17:07:21 +0100

    done until update

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const axios = require("axios");
+const { render } = require("ejs");
 
 /* GET home page */
+
+
+
 router.get("/characters", (req, res, next) => {
     axios.get("https://ih-crud-api.herokuapp.com/characters")
     .then(responseFromAPI => {
@@ -10,6 +14,52 @@ router.get("/characters", (req, res, next) => {
     })
     .catch(err => console.error(err))
 });
+
+router.get("/characters/create", (req, res)=> {
+    res.render("characters/create-character")
+});
+
+
+
+router.post("/characters/create", async (req, res)=> {
+    try {
+        const characterInfo = req.body;
+        console.log(characterInfo)
+    
+        if (req.body.debt === 'on') {
+            characterInfo.debt = true
+        } else {
+            characterInfo.debt = false
+        }
+        console.log('characterInfo modified: ', characterInfo)
+    
+        await axios.post(`https://ih-crud-api.herokuapp.com/characters`, characterInfo)
+        res.redirect('/characters');
+    }
+    catch (err) {
+        console.log(err)
+    }
+});
+
+router.get("/characters/:id/edit", async (req,res)=> {
+    const characterDetails = await axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
+    //console.log("details: ", characterDetails.data.name)
+    res.render("characters/edit-character", { character: characterDetails.data })
+})
+    
+    
+router.post("/characters/:id/edit", async(req,res)=> {
+    //console.log("req.params",req.params)
+    await axios.put(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`, {
+        name: req.body.name,
+        occupation: req.body.occupation,
+        debt: req.body.debt,
+        weapon: req.body.weapon,
+    })
+    console.log('req.params', req.params)
+    res.redirect(`/characters/${req.params.id}`)
+
+})
 
 
 router.get("/characters/:id", (req, res, next) => {
@@ -21,7 +71,10 @@ router.get("/characters/:id", (req, res, next) => {
     .catch(err => console.error(err))
 });
 
+
+
+ 
+
 module.exports = router;
 
 
-// https://ih-crud-api.herokuapp.com/characters
